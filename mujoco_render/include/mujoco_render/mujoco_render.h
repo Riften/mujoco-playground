2908cc5b1b80a1b23904c91e5d9e023e2e904c00
Riften <943275366@qq.com>
2022-01-05 23:09:25 +0800

    add render

--- mujoco_render/include/mujoco_render/mujoco_render.h
+++ mujoco_render/include/mujoco_render/mujoco_render.h
@@ -5,14 +5,34 @@
 #ifndef MUJOCO_PLAYGROUND_MUJOCO_RENDER_H
 #define MUJOCO_PLAYGROUND_MUJOCO_RENDER_H
 #include <mujoco.h>
+#include "glfw3.h"
+#include <string>
+#include <thread>
 
 namespace mujoco_render {
+    class Render; // forward decleration
+    void render_thread_fn(Render* render);
     class Render {
     public:
-        Render(mjModel* model, mjData* data);
+        Render(mjModel* model,
+               mjData* data,
+               const std::string& window_name = "Render",
+               int window_width = 1200,
+               int window_height = 900,
+               bool paused = true);
+        ~Render();
     private:
         mjModel* mj_model_;
         mjData* mj_data_;
+        mjvCamera mjv_camera_;                      // abstract camera
+        mjvOption mjv_option_;                      // visualization options
+        mjvScene mjv_scene_;                       // abstract scene
+        mjrContext mjr_context_;                     // custom GPU context
+        std::thread* render_thread_;
+        GLFWwindow* window_;
+        bool paused_;
+        /// @note argument of thread can not be reference.
+        friend void render_thread_fn(Render* render);
     };
 }
 
--- mujoco_render/src/mujoco_render.cpp
+++ mujoco_render/src/mujoco_render.cpp
@@ -5,9 +5,60 @@
 #include <mujoco_render/mujoco_render.h>
 
 namespace mujoco_render {
-    Render::Render(mjModel *model, mjData *data)
-    : mj_model_(model)
-    , mj_data_(data){
 
+
+    Render::Render(mjModel *model, mjData *data, const std::string& window_name, int window_width, int window_height, bool paused )
+    : mj_model_(model)
+    , mj_data_(data)
+    , paused_(paused){
+        window_ = glfwCreateWindow(window_width, window_height, window_name.c_str(), NULL, NULL);
+        glfwMakeContextCurrent(window_);
+        glfwSwapInterval(1);
+
+        mjv_defaultCamera(&mjv_camera_);
+        mjv_defaultOption(&mjv_option_);
+        mjv_defaultScene(&mjv_scene_);
+        mjr_defaultContext(&mjr_context_);
+
+        // create scene and context
+        // @todo make maxgeom and fontscale configurable
+        mjv_makeScene(model, &mjv_scene_, 2000);
+        mjr_makeContext(model, &mjr_context_, mjFONTSCALE_150);
+
+        render_thread_ = new std::thread(render_thread_fn, this);
+    }
+
+    Render::~Render() {
+        // Free thread
+    }
+    void render_thread_fn(Render* render) {
+        while( !glfwWindowShouldClose(render->window_) )
+        {
+            // advance interactive simulation for 1/60 sec
+            //  Assuming MuJoCo can simulate faster than real-time, which it usually can,
+            //  this loop will finish on time for the next frame to be rendered at 60 fps.
+            //  Otherwise add a cpu timer and exit this loop when it is time to render.
+            mjtNum simstart = render->mj_data_->time;
+
+            // get framebuffer viewport
+            mjrRect viewport = {0, 0, 0, 0};
+            glfwGetFramebufferSize(render->window_, &viewport.width, &viewport.height);
+
+            // update scene and render
+            mjv_updateScene(render->mj_model_, render->mj_data_, &render->mjv_option_, NULL,
+                            &render->mjv_camera_, mjCAT_ALL, &render->mjv_scene_);
+            mjr_render(viewport, &render->mjv_scene_, &render->mjr_context_);
+
+            // swap OpenGL buffers (blocking call due to v-sync)
+            glfwSwapBuffers(render->window_);
+
+            // process pending GUI events, call GLFW callbacks
+            glfwPollEvents();
+
+            if(!render->paused_) {
+                while (render->mj_data_->time - simstart < 1.0 / 60.0)
+                    mj_step(render->mj_model_, render->mj_data_);
+            }
+        }
     }
 }
